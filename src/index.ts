60e26d09b53555dd10652286468e6ec9c2f2fff8
Vallabh Wasule <120954910+Vallabh2909@users.noreply.github.com>
2025-12-05 10:33:51 +0530

    Update MongoDB connection string in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import 'dotenv/config'
 import { config } from './config';
 
-mongoose.connect(config.mongoUri).then(() => {
+mongoose.connect("mongodb+srv://demoUser:DemoPass123@cluster0.ajk9qeu.mongodb.net/demoApp").then(() => {
   console.log('MongoDB connected');
   app.listen(config.port, () => console.log(`Server running on port ${config.port}`));
 });
